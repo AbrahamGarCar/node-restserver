84d735c3c254d1adc6b745c351ed98f654a8f62c
Abraham García <abrahamgarcar@hotmail.com>
2020-09-05 19:55:18 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,10 +12,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //Base de datos
 //==========================
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-urlDB = 'mongodb+srv://Scorpio:09035286@cluster0.apbsc.mongodb.net/test?authSource=admin&replicaSet=atlas-12v244-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-// }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
